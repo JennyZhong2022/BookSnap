078f4ab97400598b8e710f69a02b161ebfc1d0a1
Yonghee Kim <19yhk91@gmail.com>
2023-11-25 18:00:27 +0900

    Structure see more button

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const BookDetail = () => {
+const BookDetail = ({ bookData }) => {
+  const style = { border: '1px solid green' };
+  const volumeData = bookData?.volumeInfo
   return (
-    <div>BookDetail</div>
-  )
-}
+    <div style={style}>
+      {bookData ? (
+        <div>
+          <img src={volumeData.imageLinks.thumbnail} alt={volumeData.title} />
+          <h2>{volumeData.title}</h2>
+          <h3>By {volumeData.authors[0]}, Published: {volumeData.publishedDate}</h3>
+          <div style={{width: "400px"}}>{volumeData.description}</div>
+        </div>
+      ) : (
+        <div>
+          <h2>BookDetail</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default BookDetail
+export default BookDetail;
