f61968c53add6c3cf74845e909987ebef0be3d91
Xiao Zhong <zhongxiao2017@gmail.com>
2023-11-27 18:29:59 +1100

    sign up UI

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,61 +1,150 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import { signUp } from '../../utilities/users-service';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default class SignUpForm extends Component {
-    state = {
-        name: '',
-        email: '',
-        password: '',
-        confirm: '',
+
+
+ // TODO remove, this demo shouldn't need to reset the theme.
+ const defaultTheme = createTheme();
+
+ export default function SignUpForm({ setUser }) {
+  const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      confirm: '',
+      error: ''
+  });
+
+  const handleChange = (event) => {
+      setFormData({
+        ...formData,
+        [event.target.name]: event.target.value,
         error: ''
-    };
+      });
+  };
 
-    _handleChange = (event) => {
-        this.setState({
-          [event.target.name]: event.target.value,
-          error: ''
-        });
-    };
+  const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+          const { name, email, password } = formData;
+          const user = await signUp({ name, email, password });
+          setUser(user);
+      } catch (err) {
+          console.error(err);
+          setFormData({ ...formData, error: 'Sign up failed - try again' });
+      }
+  };
 
-    _handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const formData = {
-                name: this.state.name,
-                email: this.state.email,
-                password: this.state.password
-            };
+  const disable = formData.password !== formData.confirm;
 
-            const user = await signUp(formData);
-            this.props.setUser(user);
-        } catch (err) {
-            // TODO: give a more meaningful error
-            console.error(err);
-            this.setState({error: 'Sign up failed - try again'});
-        }
-    }
+  return (
+ <ThemeProvider theme={defaultTheme}>
+ <Container component="main" maxWidth="xs">
+   <CssBaseline />
+   <Box
+     sx={{
+       marginTop: 8,
+       display: 'flex',
+       flexDirection: 'column',
+       alignItems: 'center',
+     }}
+   >
+     <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+       <LockOutlinedIcon />
+     </Avatar>
+     <Typography component="h1" variant="h5">
+       Sign up
+     </Typography>
+     <Box component="form" utoComplete="off" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+       <Grid container spacing={2}>
+         <Grid item xs={12} sm={12}>
+             <TextField
+             value={formData.name}
+             onChange={handleChange}
+             autoComplete="name"
+             name="name"
+             required
+             fullWidth
+             id="Name"
+             label="Name"
+             autoFocus
+           />
+         </Grid>
+         {/* <Grid item xs={12} sm={6}>
+           <TextField
+             required
+             fullWidth
+             id="lastName"
+             label="Last Name"
+             name="lastName"
+             autoComplete="family-name"
+           />
+         </Grid> */}
+         <Grid item xs={12}>
+             <TextField
+             value={formData.email}
+             onChange={handleChange}
+             required
+             fullWidth
+             id="email"
+             label="Email Address"
+             name="email"
+             autoComplete="email"
+           />
+         </Grid>
+         <Grid item xs={12}>
+             <TextField
+            value={formData.password}
+            onChange={handleChange}
+             required
+             fullWidth
+             name="password"
+             label="Password"
+             type="password"
+             id="password"
+             autoComplete="new-password"
+           />
+           </Grid>
+           
+           <Grid item xs={12}>
+             <TextField
+            name="confirm"
+            value={formData.confirm}
+            onChange={handleChange}
+             required
+             fullWidth
+             label="Confirm Password"
+             type="password"
+             id="confirm"
+             autoComplete="new-password"
+           />
+         </Grid>
 
-    render() {
-        const disable = this.state.password !== this.state.confirm;
-        return (
-          <div>
-            <div className="form-container">
-              <form autoComplete="off" onSubmit={this._handleSubmit}>
-                <label>Name</label>
-                <input type="text" name="name" value={this.state.name} onChange={this._handleChange} required />
-                <label>Email</label>
-                <input type="email" name="email" value={this.state.email} onChange={this._handleChange} required />
-                <label>Password</label>
-                <input type="password" name="password" value={this.state.password} onChange={this._handleChange} required />
-                <label>Confirm</label>
-                <input type="password" name="confirm" value={this.state.confirm} onChange={this._handleChange} required />
-                <button type="submit" disabled={disable}>SIGN UP</button>
-              </form>
-            </div>
-            <p className="error-message">&nbsp;{this.state.error}</p>
-          </div>
-        );
-    }
-}
+        
+       </Grid>
+         <Button
+        disabled={disable}
+         type="submit"
+         fullWidth
+         variant="contained"
+         sx={{ mt: 3, mb: 2 }}
+       >
+         Sign Up
+       </Button>
+       
+     </Box>
+   </Box>
 
+ </Container>
+</ThemeProvider>
+)}
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,27 +1,53 @@
 import { useState } from 'react';
-
+import Grid from '@mui/material/Grid'; // Import Grid if not already imported
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import Container from '@mui/material/Container';
 
-const AuthPage = ({setUser}) => {
+function Copyright() {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" >
+       
+        <p color="inherit" >
+        {'Copyright © '}
+          Book Snap{' '}
+          {new Date().getFullYear()}
+        {'.'}
+        </p>
+        
+      </Typography>
+    );
+  }
+
+const AuthPage = ({ setUser }) => {
     const [showSignUp, setShowSignUp] = useState(false);
     return (
-        <div>
-            {
-                showSignUp ?
-                <SignUpForm setUser={ setUser } />
-                :
-                <LoginForm setUser={ setUser } />
-            }
-            <button onClick={ () => setShowSignUp(!showSignUp) }>
-                {
-                    showSignUp ?
-                    'Login'
-                    :
-                    'Sign Up'
-                }
-            </button>
-        </div>
+        <Container component="main" maxWidth="xs">
+        <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+            {showSignUp ? (
+                <SignUpForm setUser={setUser} />
+            ) : (
+                <LoginForm setUser={setUser} />
+            )}
+            <Grid container justifyContent="flex-end">
+                <Grid item>
+                    <Link href="#" onClick={() => setShowSignUp(!showSignUp)} variant="body2">
+                        {showSignUp ? 'Already have an account? Sign in' : 'No Account? Sign Up'}
+                    </Link>
+                </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+            </Box>
+            </Container>
     );
 }
 
