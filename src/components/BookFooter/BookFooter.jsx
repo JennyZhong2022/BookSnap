dcb5f4b55050030de97261479f173bdaab347972
Xiao Zhong <zhongxiao2017@gmail.com>
2023-11-28 11:16:35 +1100

    update footer

--- src/components/BookFooter/BookFooter.jsx
+++ src/components/BookFooter/BookFooter.jsx
@@ -1,13 +1,17 @@
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 
 const Footer = () => {
   return (
+    <Box  sx={{ mt: 3 }}>
     <Typography variant="body2" color="text.secondary" align="center" component="footer">
-      {'Copyright © '}
-        Book Snap{' '}
+    
+        BookSnap  {'©'} by Annabelle Jenny Phuong Yonghee {' '}
         {new Date().getFullYear()}
       {'.'}
-    </Typography>
+      </Typography>
+    </Box>
   );
 }
 
