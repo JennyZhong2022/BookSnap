ceeb99f84c652bd4c156acec932a6db82e6e724a
Phuong Vu <phuongmous@gmail.com>
2023-11-29 10:28:42 +1100

    MUI for My Books Page and NavBar

--- src/components/MyBooksList/MyBooksList.jsx
+++ src/components/MyBooksList/MyBooksList.jsx
@@ -3,7 +3,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 const MyBooksList = ({ myBooks, onDeleteBook }) => {
   return (
-    <div style={{margin: '1em auto', width: '50%'}}>
+    <div style={{margin: '1em auto', width: '80%'}}>
       {myBooks.map((book) => (
         <MyBooksListItem key={book._id} book={book} onDeleteBook={onDeleteBook} />
       ))}
